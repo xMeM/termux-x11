7856e15f73719770c70be23cce5c527009467ca2
xMeM <haooy@outlook.com>
2024-07-11 21:48:08 +0800

    Backport glamor GLES fixes

--- app/src/main/cpp/recipes/xserver.cmake
+++ app/src/main/cpp/recipes/xserver.cmake
@@ -282,6 +282,7 @@ target_link_options(Xlorie PRIVATE "-Wl,--as-needed" "-Wl,--no-undefined" "-fvis
 target_link_libraries(Xlorie "-Wl,--whole-archive" ${XSERVER_LIBS} "-Wl,--no-whole-archive" android log m z)
 target_compile_options(Xlorie PRIVATE ${compile_options})
 target_apply_patch(Xlorie "${CMAKE_CURRENT_SOURCE_DIR}/xserver" "${CMAKE_CURRENT_SOURCE_DIR}/patches/xserver.patch")
+target_apply_patch(Xlorie "${CMAKE_CURRENT_SOURCE_DIR}/xserver" "${CMAKE_CURRENT_SOURCE_DIR}/patches/xserver-1636.patch")
 
 add_library(lorie SHARED "lorie/activity.c")
 target_include_directories(lorie PRIVATE ${inc})
