4dc9f0d47fe3f6675d61e7d7b5ba9d1ae4f3296f
xMeM <haooy@outlook.com>
2025-02-16 13:44:26 +0800

    Implement opaque FD import

--- app/src/main/cpp/lorie/glamor_egl_android.h
+++ app/src/main/cpp/lorie/glamor_egl_android.h
@@ -1,6 +1,7 @@
 #include "lorie.h"
-#include "pixmap.h"
+
 #include "buffer.h"
+#include "pixmap.h"
 
 Bool glamor_egl_init(ScreenPtr screen);
 
@@ -9,5 +10,8 @@ PixmapPtr glamor_egl_create_pixmap(
 
 PixmapPtr glamor_egl_create_pixmap_from_ahardware_buffer(
    ScreenPtr screen, int depth, AHardwareBuffer *ahardware_buffer);
+
+PixmapPtr glamor_egl_create_pixmap_from_opaque_fd(ScreenPtr screen, int w,
+   int h, int depth, size_t size, uint32_t offset, int fd);
 
 LorieBuffer *glamor_egl_get_pixmap_lorie_buffer(PixmapPtr pixmap);
